d110052a84a070da44d6d418784c5dfe642ed0ff
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-03-25 18:28:46 -0400

    olane deosn't load

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
   render() {
     return (
         <div className="main">
-              <p className = "description"> <span> A place for you to ask questions and get help about transitioning to your new home. </span> </p>
+              <img src = {require("../photos/plane.jpg" id = "bg")} />
         </div>
     );
   }
