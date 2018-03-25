e50925742c5ba092a45c25d2adb602ba0104ce62
Extremely Penguin <extremelypenguinnnn@gmail.com>
2018-03-25 15:54:05 -0400

    updated landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,17 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <p className = "thisishome"> This is home </p>
+        <p className = "thisishome"> This is Immitia. </p>
+<p> logo here </p>
+<p> logo here </p>
+<p> logo here </p>
+<p> logo here </p>
+<p> logo here </p>
+<p> logo here </p>
+
+
+    <div className = "bigLine"/>
+    <p className = "description"> <span> A place for you to ask questions and get help about transitioning to your new home. </span> </p>
       </div>
     );
   }
